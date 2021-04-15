f6f2ed5bb0b7841d9a615378020d6ffeb21a29aa
William Frazier <will.frazier@gmail.com>
2021-04-15 22:24:27 +0100

    stock quotes finally work

--- apis/rapid.js
+++ apis/rapid.js
@@ -1,15 +1,13 @@
 var unirest = require("unirest");
 
 const { rapid: { apiKey, symbols } } = require('../config')
-console.log(apiKey)
-console.log(symbols.join(', '))
 
 module.exports = {
-  quotes: () => {
+  quote: (symbol) => new Promise((resolve, reject) => {
     var req = unirest("GET", "https://twelve-data1.p.rapidapi.com/quote");
 
     req.query({
-      "symbol": "AMZN",
+      "symbol": symbol,
       "interval": "5min",
       "format": "json",
       "outputsize": "30"
@@ -21,29 +19,11 @@ module.exports = {
       "useQueryString": true
     });
 
-    return new Promise((resolve, reject) => {
-      req.end(function (res) {
-        if (res.error) {
-          return reject(res.error)
-        }
-        resolve(res.body)
-      })
+    req.end(function (res) {
+      if (res.error) {
+        return reject({ error: `Error: failed to fetch rapid quotes - ${res.error}` })
+      }
+      resolve({ data: res.body })
     })
-    
-  },
-  quotes1: () => request({
-    method: 'GET',
-    uri: 'https://twelve-data1.p.rapidapi.com/quote',
-    qs: {
-      'symbol': symbols.join(','),
-      'interval': '5min',
-      'format': 'json',
-      'outputsize': '30',
-    },
-    headers: {
-      'x-rapidapi-key': apiKey,
-      'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
-      'useQueryString': true,
-    },
-  }).catch(error => ({ error: `Error: fetching rapi quotes failed -- ${error}` }))
+  }),
 }
--- btcTicker.js
+++ btcTicker.js
@@ -2,28 +2,30 @@ const coin = require('./apis/coinMarketCap')
 const rapid = require('./apis/rapid')
 const oled = require('./apis/oled')
 
-const getFormattedQuote = (coinObj) =>
+const formatCoinQuote = (coinObj) =>
   parseFloat(coinObj.quote.USD.price).toFixed(2)
+const formatRapidQuote = (rapidData) =>
+  parseFloat(rapidData.open).toFixed(2)
 
 const main = async () => {
   // const { error, data = [] } = await coin.quotes()
-  const { error, data = [] } = await rapid.quotes()
-  console.log(data)
-  console.log(error)
+  const { error, data = [] } = await rapid.quote("TSLA")
+  // console.log(data)
+  // console.log(error)
   
 //   for (const [key, value] of Object.entries(data)) {
 //     console.log('Starting ticker run.')
 //     const results = await oled.scrollingLrg({
-//       text: `${value.symbol}: ${getFormattedQuote(value)}`,
+//       text: `${value.symbol}: ${formatCoinQuote(value)}`,
 //     })
 //     console.log(results)
 //   }
-//   console.log('Starting fixedTwoRows')
-//   const results = await oled.fixedTwoRows({
-//     r1Text: "BTC:",
-//     r2Text: getFormattedQuote(data.BTC),
-//   })
-//   console.log(results)
+  console.log('Starting fixedTwoRows')
+  const results = await oled.fixedTwoRows({
+    r1Text: "TSLA:",
+    r2Text: formatRapidQuote(data),
+  })
+  console.log(results)
 }
 
 main()
